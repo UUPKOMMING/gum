4946836ddbf5711d8bc4832bd360a42796397d1d
UUPKOMMING <55725065+UUPKOMMING@users.noreply.github.com>
2020-09-22 04:09:13 +0000

    Add files via upload

---
+++ build/precache-manifest.178cc7e6ee9d5cd88075340911e46c8d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "054c6712abd79a7b0a12fa9154df93c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93b4a4fa33548bdbd861",
+    "url": "/static/css/2.df83caf4.chunk.css"
+  },
+  {
+    "revision": "e0a714f14c27b3196cc0",
+    "url": "/static/css/main.d15a0921.chunk.css"
+  },
+  {
+    "revision": "93b4a4fa33548bdbd861",
+    "url": "/static/js/2.adb83ca0.chunk.js"
+  },
+  {
+    "revision": "9a9e56582fbb6743dfc4c1baab3b2d6f",
+    "url": "/static/js/2.adb83ca0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0a714f14c27b3196cc0",
+    "url": "/static/js/main.d1f61387.chunk.js"
+  },
+  {
+    "revision": "a7aa1bfeb9788594e513",
+    "url": "/static/js/runtime-main.d74b4374.js"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "1bb027109345a90a9eab1e929d8669c2",
+    "url": "/static/media/iconmonstr-github-1.1bb02710.svg"
+  },
+  {
+    "revision": "341a67c538d67f9ce92005cf14255dc2",
+    "url": "/static/media/iconmonstr-instagram-11.341a67c5.svg"
+  },
+  {
+    "revision": "c45df616ed13f1df0839e87c3704555d",
+    "url": "/static/media/iconmonstr-whatsapp-1.c45df616.svg"
+  },
+  {
+    "revision": "d186798348f1f3efbf26db9c94708ab3",
+    "url": "/static/media/pic1.d1867983.jpg"
+  },
+  {
+    "revision": "d4c0ebe5e54cbc0be8a1bb3f36ddacee",
+    "url": "/static/media/pic2.d4c0ebe5.jpg"
+  },
+  {
+    "revision": "d6a82c2c49268cfe170e550182546b53",
+    "url": "/static/media/pic3.d6a82c2c.jpg"
+  },
+  {
+    "revision": "e350c97994e2c98006cd21152a31f6a6",
+    "url": "/static/media/pic4.e350c979.jpg"
+  },
+  {
+    "revision": "cf9c1d3f8300d5dca83fccf79bfddd2b",
+    "url": "/static/media/pic5.cf9c1d3f.jpg"
+  }
+]);
